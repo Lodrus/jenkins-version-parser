0c264bef3d5ddbf5a2e14ba420f2a0f30df756e3
Lodra <ramsay.ac@gmail.com>
2022-01-05 18:45:13 -0500

    Added column output and arguments for various data

--- main.go
+++ main.go
@@ -8,28 +8,80 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"text/tabwriter"
 )
 
 func main() {
 	sourceUrl := "https://updates.jenkins.io/update-center.actual.json"
 
+	defer handleErrors()
+
 	// CLI Arguments
-	// var buildDateFlag = flag.Bool("b", false, "Print the build date")
-	// var delimeterFlag = flag.String("d", ",", "The delimeter used to separate returned data")
-	// var nameFlag = flag.Bool("n", false, "Print the plugin name")
-	// var pluginsFlag = flag.String("p", "", "A comma-separated list of plugin names to return information for. Use * to list all plugins")
-	// var sizeFlag = flag.Bool("s", false, "Print the size in bytes")
-	// var sha1Flag = flag.Bool("sha1", false, "Print the sha1")
-	// var sha256Flag = flag.Bool("sha256", false, "Print the sha256")
-	// var urlFlag = flag.Bool("u", false, "Print the download url")
-	// var versionFlag = flag.Bool("v", false, "Print the version")
+	PrintDelimeterFlag := flag.String("d", "", "The delimeter used to separate returned data. Prints table output by default")
+	headerFlag := flag.Bool("h", true, "Print the header row")
+	// PluginsFlag := flag.String("p", "", "A comma-separated list of plugin names to return information for. Use * to list all plugins")
+
+	type column struct {
+		displayName string
+		enabled     *bool
+		keyName     string
+	}
+
+	columns := []column{
+		{"NAME", flag.Bool("n", false, "Print the plugin name"), "name"},
+		{"VERSION", flag.Bool("v", false, "Print the version"), "version"},
+		{"SIZE", flag.Bool("s", false, "Print the size in bytes"), "size"},
+		{"SHA1", flag.Bool("sha1", false, "Print the sha1"), "sha1"},
+		{"SHA256", flag.Bool("sha256", false, "Print the sha256"), "sha256"},
+		{"BUILD DATE", flag.Bool("b", false, "Print the build date"), "buildDate"},
+		{"URL", flag.Bool("u", true, "Print the download url"), "url"},
+	}
 	flag.Parse()
 
-	defer handleErrors()
+	// We only care about columns that are enabled so we filter them early and save some iterations and conditionals later
+	columnsFiltered := []column{}
+	for _, col := range columns {
+		if *col.enabled {
+			columnsFiltered = append(columnsFiltered, col)
+		}
+	}
+	if len(columnsFiltered) == 0 {
+		panic("The command requires at least one piece of data should be returned")
+	}
+	if len(columnsFiltered) == 1 {
+		*headerFlag = false
+	}
 
 	data := getUpdateData(sourceUrl)
 
-	fmt.Println(data["core"].(map[string]interface{})["url"])
+	// setup the stdout writer using the tabwriter package
+	var delimeter string
+	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 4, ' ', 0)
+	delimeter = "\t"
+	defer writer.Flush()
+	if *PrintDelimeterFlag != "" {
+		// We still need a tabwriter object but this one will not add any spacing. Just a simple delimeter
+		writer = tabwriter.NewWriter(os.Stdout, 0, 0, 0, []byte(*PrintDelimeterFlag)[0], tabwriter.AlignRight)
+		delimeter = *PrintDelimeterFlag
+	}
+
+	// Header Row
+	var header string
+	if *headerFlag {
+		for _, col := range columnsFiltered {
+			header += col.displayName + delimeter
+		}
+		fmt.Fprintln(writer, strings.TrimSuffix(header, delimeter))
+	}
+
+	// Print data for the main jenkins.war
+	var buffer string
+	core := data["core"].(map[string]interface{})
+	for _, col := range columnsFiltered {
+		buffer += fmt.Sprint(core[col.keyName]) + delimeter
+	}
+	fmt.Fprintln(writer, strings.TrimSuffix(buffer, delimeter))
 }
 
 func handleErrors() {
